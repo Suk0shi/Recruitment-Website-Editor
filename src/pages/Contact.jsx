aeb70e5d201ad829a838278d27c80e24a1ee5ed2
Sukoshi <hugo.cordon100@gmail.com>
2025-01-17 17:50:44 +0000

    Update contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,59 @@
 import '../styles/Contact.css'
+import { useState } from 'react';
 import Header from '../components/Header'
 
 function Contact() {
+
+  const [message, setMessage] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const payload = Object.fromEntries(formData);
+  
+  fetch(`${import.meta.env.VITE_API_URL}/jobs/sendEmail`, {
+    method: 'Post', 
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload),
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  })
+  .then((response) => {
+    setMessage(response);
+    if (response === 'Email sent successfully!') {
+      e.target.reset();
+    }
+  })
+  .catch((err) => {
+    setMessage(err.toString());
+  });
+}
 
   return (
     <>
       <Header/>
       <h1>Contact</h1>
-      <form action="">
+      <form action="POST" onSubmit={handleSubmit}>
         <label htmlFor="name">*Name</label>
-        <input type="text" name="name" id="name" required/>
+        <input type="text" name="name" required/>
         <label htmlFor="email">*Email</label>
-        <input type="email" name="email" id="email" required/>
+        <input type="email" name="email" required/>
         <label htmlFor="subject">Subject</label>
-        <input type="text" name="subject" id="subject"/>
+        <input type="text" name="subject"/>
         <label htmlFor="message">Message</label>
         <textarea name="message" id="message"></textarea>
-        <button type='submit'>Send</button>
+        <button>Send</button>
       </form>
+      <p>{message}</p>
     </>
   )
 }
