f473d863a97a75ed66f0b6b2a29486f4220ff41f
Sukoshi <hugo.cordon100@gmail.com>
2025-02-04 13:11:05 +0000

    Fix individual job info dissapearing on refresh

--- src/pages/IndividualOpportunity.jsx
+++ src/pages/IndividualOpportunity.jsx
@@ -2,11 +2,15 @@ import '../styles/IndividualOpportunity.css'
 import Header from '../components/Header'
 import { Link, useNavigate } from "react-router-dom";
 import ContactForm from '../components/ContactForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function IndividualOpportunity({postInfo}) {
 
   const [showForm, setShowForm] = useState(false);
+  const [storedPostInfo, setStoredPostInfo] = useState(() => {
+    const savedPost = localStorage.getItem("postInfo");
+    return savedPost ? JSON.parse(savedPost) : null;
+  });
   
   const navigate = useNavigate();
   
@@ -19,6 +23,15 @@ function IndividualOpportunity({postInfo}) {
     } 
   }
 
+  useEffect(() => {
+    if (postInfo.date !== undefined) {
+      setStoredPostInfo(postInfo);
+      localStorage.setItem("postInfo", JSON.stringify(postInfo));
+    }
+    
+  }, [postInfo])
+  
+
   return (
     <div className='page'>
       <Header/>
@@ -26,10 +39,10 @@ function IndividualOpportunity({postInfo}) {
         <div className='opportunitiesCards'>
           <div className='individualPostCard'>
             <button onClick={handleBack} className='backButton'>Back</button>
-            <h3>{postInfo.title}</h3>
-            <p className='location'>{postInfo.location}</p>
-            <p className='date'>{postInfo.date}</p>
-            <p className='text'>{postInfo.text}</p>
+            <h3>{storedPostInfo.title}</h3>
+            <p className='location'>{storedPostInfo.location}</p>
+            <p className='date'>{storedPostInfo.date}</p>
+            <p className='text'>{storedPostInfo.text}</p>
             <p>To apply for this job...</p>
             <a onClick={() => setShowForm(!showForm)}>Contact Us</a>
             {showForm ? <div className="contactMini">
